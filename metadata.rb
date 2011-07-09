8f0e40aa7ba858848f60631aa4dd474881c04cc0
Gerald L. Hevener Jr. M.S <hevenerg@marshall.edu>
2011-07-08 20:53:16 -0400

    added metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Gerald L. Hevener Jr., M.S."
 maintainer_email "jackl0phty@gmail.com"
-license          ""
+license          "Apache License, Version 2.0"
 description      "Installs/Configures snmp-rndc-stats"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
